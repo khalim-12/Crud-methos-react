501aaccfd3869190c38b7043d0eac0f426e5ef8b
khalim-12 <83351430+khalim-12@users.noreply.github.com>
2023-01-01 00:17:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+ import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+const root2 = ReactDOM.createRoot(document.getElementById('root'));
+root2.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+reportWebVitals();
+
+
